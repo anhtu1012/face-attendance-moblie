9fb5bba6dcc4810913a37e3951ea74fb39b23855
khangttse182098 <khangthaitran246@gmail.com>
2025-10-20 21:45:58 +0700

    feat: add more animation for drawer

--- components/ui/CustomDrawer.tsx
+++ components/ui/CustomDrawer.tsx
@@ -1,4 +1,10 @@
-import { Entypo, Feather, FontAwesome6, Ionicons } from "@expo/vector-icons";
+import {
+  Entypo,
+  Feather,
+  FontAwesome6,
+  Ionicons,
+  MaterialIcons,
+} from "@expo/vector-icons";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import {
   DrawerContentComponentProps,
@@ -49,6 +55,7 @@ const CustomDrawer = ({ navigation, state }: DrawerContentComponentProps) => {
   const animatedStyles = state.routes.map((_, index) =>
     useAnimatedItemStyle(distance, index),
   );
+  animatedStyles.push(useAnimatedItemStyle(distance, 5));
 
   const handleLogout = useCallback(async () => {
     try {
@@ -72,7 +79,7 @@ const CustomDrawer = ({ navigation, state }: DrawerContentComponentProps) => {
         },
         {
           label: "Chat App",
-          icon: <Entypo name="chat" size={24} color="#fff" />,
+          icon: <Ionicons name="chatbubble-outline" size={24} color="#fff" />,
           route: "/(drawer)/chat",
         },
         {
@@ -87,7 +94,7 @@ const CustomDrawer = ({ navigation, state }: DrawerContentComponentProps) => {
         },
         {
           label: "Thông báo",
-          icon: <Ionicons name="notifications" size={24} color="#fff" />,
+          icon: <Feather name="bell" size={24} color="#fff" />,
           route: "/(drawer)/notifications",
         },
       ].map((item, index) => (
@@ -103,9 +110,15 @@ const CustomDrawer = ({ navigation, state }: DrawerContentComponentProps) => {
       ))}
 
       {/* Logout Button */}
-      <Pressable onPress={handleLogout} style={styles.logoutButton}>
-        <Text style={styles.logoutText}>Đăng xuất</Text>
-      </Pressable>
+      <Animated.View style={animatedStyles[5]}>
+        <Pressable
+          onPress={handleLogout}
+          style={[styles.itemContainer, { marginTop: 20 }]}
+        >
+          <MaterialIcons name="logout" size={24} color="#fff" />
+          <Text style={styles.itemText}>Đăng xuất</Text>
+        </Pressable>
+      </Animated.View>
     </View>
   );
 };
--- components/ui/DrawerScreenWrapper.tsx
+++ components/ui/DrawerScreenWrapper.tsx
@@ -1,12 +1,31 @@
 import { useDrawerProgress } from "@react-navigation/drawer";
+import { useEffect } from "react";
 import { StyleSheet } from "react-native";
 import Animated, {
   interpolate,
+  useAnimatedReaction,
   useAnimatedStyle,
+  useSharedValue,
+  withSpring,
 } from "react-native-reanimated";
 
 const DrawerScreenWrapper = ({ children }: { children: any }) => {
   const progress = useDrawerProgress(); // Shared value from drawer
+  const distance = useSharedValue(0);
+
+  useAnimatedReaction(
+    () => progress.value,
+    (currentProgress) => {
+      if (currentProgress > 0.5) {
+        distance.value = withSpring(1, {
+          damping: 15,
+          stiffness: 120,
+        });
+      } else {
+        distance.value = 0;
+      }
+    },
+  );
 
   // Animated style that reacts to drawer progress
   const animatedStyle = useAnimatedStyle(() => {
@@ -15,15 +34,28 @@ const DrawerScreenWrapper = ({ children }: { children: any }) => {
         {
           scale: interpolate(progress.value, [0, 1], [1, 0.75], "clamp"),
         },
+        {
+          translateX: interpolate(distance.value, [0, 1], [0, 9]),
+        },
       ],
       borderRadius: interpolate(progress.value, [0, 1], [0, 30]),
       overflow: "hidden",
     };
   });
 
+  const animatedBehindStyle = useAnimatedStyle(() => {
+    return {
+      transform: [
+        {
+          translateX: interpolate(distance.value, [0, 1], [25, 30]),
+        },
+      ],
+    };
+  });
+
   return (
     <>
-      <Animated.View style={styles.behindPage} />
+      <Animated.View style={[styles.behindPage, animatedBehindStyle]} />
       <Animated.View style={[styles.container, animatedStyle]}>
         {children}
       </Animated.View>
