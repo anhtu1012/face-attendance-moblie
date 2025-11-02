3321ddce0ed3cc5fb5626bc4420730fb449ad146
khangttse182098 <khangthaitran246@gmail.com>
2025-11-02 15:28:38 +0700

    feat: add api call for current date timekeep

--- services/timesheet/api.ts
+++ services/timesheet/api.ts
@@ -1,6 +1,30 @@
 import api from "@/config/axios";
 import { dtoPutTimekeep } from "@/models/timesheet/dtoTimekeep";
 
-export const timkeep = async (data: dtoPutTimekeep) => {
-  return api.put("time-keeping/danh-sach-cham-cong", data);
+export const timkeep = async (data: dtoPutTimekeep, timekeepingId: string) => {
+  return api.put(`time-keeping/cham-cong${timekeepingId}`, data);
+};
+
+export const getCurrentTimekeepingData = async (userId: string) => {
+  const today = new Date();
+  const yesterday = new Date(today);
+
+  // set yesterday
+  yesterday.setDate(yesterday.getDate() - 1);
+  yesterday.setUTCHours(0, 0, 0, 0);
+
+  // set Today
+  today.setUTCHours(0, 0, 0, 0);
+
+  console.log("startTime: ", yesterday.toISOString());
+  console.log("endTime: ", today.toISOString());
+  console.log("userId: ", userId);
+
+  return api.get("time-keeping/danh-sach-cham-cong", {
+    params: {
+      startTime: yesterday.toISOString(),
+      endTime: today.toISOString(),
+      userId: userId,
+    },
+  });
 };
