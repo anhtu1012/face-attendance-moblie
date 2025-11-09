c492f8cdfbcc45afbfd35a80f28f7bbe77e852c7
phucphse181514 <phucphse181514@fpt.edu.vn>
2025-11-09 14:29:28 +0700

    feat: merge develop

--- components/Timekeeping/TimesheetCalendar.tsx
+++ components/Timekeeping/TimesheetCalendar.tsx
@@ -6,7 +6,7 @@ import "dayjs/locale/vi";
 import localeData from "dayjs/plugin/localeData";
 import weekday from "dayjs/plugin/weekday";
 import React, { useMemo, useState } from "react";
-import { RefreshControl, ScrollView, StyleSheet, View } from "react-native";
+import { ActivityIndicator, RefreshControl, ScrollView, StyleSheet, View } from "react-native";
 import { useSelector } from "react-redux";
 import TimekeepingBox from "./TimekeepingBox";
 import TimekeepingModal from "./TimekeepingModal";
@@ -130,7 +130,11 @@ export default function TimesheetCalendar() {
     }
   };
 
-  return (
+  return isLoadingTimekeeping ? (
+    <View>
+      <ActivityIndicator size="small" color="#3674B5" />
+    </View>
+  ) : (
     <ScrollView
       refreshControl={
         <RefreshControl
